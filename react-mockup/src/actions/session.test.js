95ba3de666bbeb852244b8e62a3283066d809ee0
Paige Gorry <paigeegorry@gmail.com>
2019-03-14 09:34:59 -0700

    all tests passing

--- react-mockup/src/actions/session.test.js
+++ react-mockup/src/actions/session.test.js
@@ -1,14 +1,17 @@
-import { setSession, SET_SESSION } from './session';
+import { setSession } from './session';
 
-jest.mock('../services/auth.js', () => ({}));
+jest.mock('../services/auth.js');
 
 describe('session actions', () => {
   it('can handle the auth', () => {
     const action = setSession();
 
     expect(action).toEqual({
-      type: SET_SESSION,
-      payload: Promise.resolve()
+      payload: Promise.resolve(),
+      fulfilledType: 'SET_SESSION_FULFILLED',
+      pendingType: 'SET_SESSION_PENDING',
+      rejectedType: 'SET_SESSION_REJECTED',
+      type: 'SET_SESSION'
     });
   });
 });
---
+++ react-mockup/src/services/__mocks__/auth.js
@@ -0,0 +1,11 @@
+export const login = () => {};
+
+export const handleAuth = () => {
+  return Promise.resolve({
+    token: 'ektjbnr',
+    handle: 'lefnvg',
+    id: 'efjlnger',
+    profilePicture: 'erjgnelrkjf'
+  });
+};
+
