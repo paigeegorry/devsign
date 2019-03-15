a932fb99f39b430d684bd454ffc8f909ca7d4f16
Paige Gorry <paigeegorry@gmail.com>
2019-03-15 14:32:26 -0700

    trying to connect the front and back end

--- react-mockup/src/components/app/App.js
+++ react-mockup/src/components/app/App.js
@@ -5,7 +5,6 @@ import HumFeed from '../hums/HumFeed';
 import UserDetail from '../user/UserDetail';
 import Callback from '../../containers/auth/Callback';
 import { withSession } from '../../containers/auth/withSession';
-import Header from './Header';
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
         <Route exact path="/" component={withSession(Home)} />
         <Route exact path="/hum-feed" component={withSession(HumFeed)} />
         <Route exact path="/profile" component={withSession(UserDetail)} />
-        <Route exact path="/header" component={Header} />
         <Route exact path="/callback" component={Callback} />
       </Switch>
     </Router>
--- react-mockup/src/services/hums.js
+++ react-mockup/src/services/hums.js
@@ -1,8 +1,14 @@
 export const getHums = () => {
-  return Promise.resolve([
-    { id: 1234, handle: 'username1', hum: 'this is my first hum', img: 'react-mockup/src/assets/user-image.png' },
-    { id: 1235, handle: 'username2', hum: 'this is my second hum', img: 'react-mockup/src/assets/user-image.png' },
-    { id: 1236, handle: 'username3', hum: 'this is my third hum', img: 'react-mockup/src/assets/user-image.png' },
-    { id: 1237, handle: 'username4', hum: 'this is my fourth hum', img: 'react-mockup/src/assets/user-image.png' }
-  ]);
+  return fetch(process.env.API_URL)
+    .then(res => [res.ok, res.json()])
+    .then(([ok, json]) => {
+      if(!ok) throw json;
+      return json;
+    });
+  // return Promise.resolve([
+  //   { id: 1234, handle: 'username1', hum: 'this is my first hum', img: 'react-mockup/src/assets/user-image.png' },
+  //   { id: 1235, handle: 'username2', hum: 'this is my second hum', img: 'react-mockup/src/assets/user-image.png' },
+  //   { id: 1236, handle: 'username3', hum: 'this is my third hum', img: 'react-mockup/src/assets/user-image.png' },
+  //   { id: 1237, handle: 'username4', hum: 'this is my fourth hum', img: 'react-mockup/src/assets/user-image.png' }
+  // ]);
 };
