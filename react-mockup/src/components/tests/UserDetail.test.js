2d165d4e5619414dbb568d672de8549277fb6327
Paige Gorry <paigeegorry@gmail.com>
2019-03-15 09:10:18 -0700

    all tests passing

--- react-mockup/src/components/tests/UserDetail.test.js
+++ react-mockup/src/components/tests/UserDetail.test.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import UserDetail from '../user/UserDetail';
 import { MemoryRouter } from 'react-router';
+import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+jest.mock('../../services/auth.js');
 
 describe('UserDetail snapshot', () => {
   it('can match a snapshot', () => {
-    const tree = renderer.create(
+    const tree = shallow(
       <MemoryRouter>
         <UserDetail />
       </MemoryRouter>
-    ).toJSON();
+    );
     expect(tree).toMatchSnapshot();
   });
 });
--- react-mockup/src/components/tests/UserInfo.test.js
+++ react-mockup/src/components/tests/UserInfo.test.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import UserInfo from '../user/UserInfo';
 import { MemoryRouter } from 'react-router';
+import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+jest.mock('../../services/auth.js');
+
+const handle = 'handle';
+const profilePicture = 'https://res.cloudinary.com/dkrup6iyl/image/fetch/v1552429847/https://res.cloudinary.com/dkrup6iyl/image/upload/v1552429521/download.png';
 
 describe('UserInfo snapshot', () => {
   it('can match a snapshot', () => {
-    const tree = renderer.create(
+    const tree = shallow(
       <MemoryRouter>
-        <UserInfo />
+        <UserInfo handle={handle} profilePicture={profilePicture} />
       </MemoryRouter>
-    ).toJSON();
+    );
     expect(tree).toMatchSnapshot();
   });
 });
