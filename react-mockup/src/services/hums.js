56a552a645157d8efc8ebd9a6ae447e0885f3f96
Paige Gorry <paigeegorry@gmail.com>
2019-03-15 15:39:47 -0700

    cors error fix

--- react-mockup/src/services/hums.js
+++ react-mockup/src/services/hums.js
@@ -1,14 +1,11 @@
-export const getHums = () => {
-  return fetch(process.env.API_URL)
-    .then(res => [res.ok, res.json()])
-    .then(([ok, json]) => {
-      if(!ok) throw json;
-      return json;
-    });
-  // return Promise.resolve([
-  //   { id: 1234, handle: 'username1', hum: 'this is my first hum', img: 'react-mockup/src/assets/user-image.png' },
-  //   { id: 1235, handle: 'username2', hum: 'this is my second hum', img: 'react-mockup/src/assets/user-image.png' },
-  //   { id: 1236, handle: 'username3', hum: 'this is my third hum', img: 'react-mockup/src/assets/user-image.png' },
-  //   { id: 1237, handle: 'username4', hum: 'this is my fourth hum', img: 'react-mockup/src/assets/user-image.png' }
-  // ]);
-};
+import { get } from './request';
+
+export const getHums = () => get('/tweets');
+
+
+// return Promise.resolve([
+//   { id: 1234, handle: 'username1', hum: 'this is my first hum', img: 'react-mockup/src/assets/user-image.png' },
+//   { id: 1235, handle: 'username2', hum: 'this is my second hum', img: 'react-mockup/src/assets/user-image.png' },
+//   { id: 1236, handle: 'username3', hum: 'this is my third hum', img: 'react-mockup/src/assets/user-image.png' },
+//   { id: 1237, handle: 'username4', hum: 'this is my fourth hum', img: 'react-mockup/src/assets/user-image.png' }
+// ]);
---
+++ react-mockup/src/services/request.js
@@ -0,0 +1,21 @@
+import store from '../store';
+import { getToken } from '../selectors/session';
+
+export const request = (path, method, body) => {
+  return fetch(`${process.env.API_URL}/${path}`, {
+    method,
+    body: body ? JSON.stringify(body) : null,
+    headers: {
+      Authorization: `Bearer ${getToken(store.getState())}`
+    }
+  })
+    .then(res => [res.ok, res.json()])
+    .then(([ok, json]) => {
+      if(!ok) throw json;
+      return json;
+    });
+};
+
+export const post = (path, body) => request(path, 'POST', body);
+export const get = path => request(path, 'GET');
+export const del = (path, body) => request(path, 'DELETE', body);
