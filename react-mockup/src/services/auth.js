d8dcb5ad48ed2f8d9b041bb41f45382b9ecb1e9e
Paige Gorry <paigeegorry@gmail.com>
2019-03-15 16:09:01 -0700

    checkout netlify deployment

--- react-mockup/src/services/auth.js
+++ react-mockup/src/services/auth.js
@@ -3,7 +3,7 @@ import { WebAuth } from 'auth0-js';
 const auth0 = new WebAuth({
   clientID: process.env.AUTH0_CLIENT_ID,
   domain: process.env.AUTH0_DOMAIN,
-  redirectUrl: process.env.AUTH0_REDIRECT,
+  redirectUri: process.env.AUTH0_REDIRECT,
   responseType: 'token id_token',
   scope: 'openid profile'
 });
--- react-mockup/src/services/hums.js
+++ react-mockup/src/services/hums.js
@@ -1,11 +1,3 @@
 import { get } from './request';
 
 export const getHums = () => get('hums');
-
-
-// return Promise.resolve([
-//   { id: 1234, handle: 'username1', hum: 'this is my first hum', img: 'react-mockup/src/assets/user-image.png' },
-//   { id: 1235, handle: 'username2', hum: 'this is my second hum', img: 'react-mockup/src/assets/user-image.png' },
-//   { id: 1236, handle: 'username3', hum: 'this is my third hum', img: 'react-mockup/src/assets/user-image.png' },
-//   { id: 1237, handle: 'username4', hum: 'this is my fourth hum', img: 'react-mockup/src/assets/user-image.png' }
-// ]);
